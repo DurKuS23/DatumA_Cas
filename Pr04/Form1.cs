1f03ab4729bd33a2eba18be2bf74a1bc33e3a26f
Daniel Nováček <Danny230147@seznam.cz>
2022-11-07 17:13:16 +0100

    Pr4_ENDED

--- Pr04/Form1.cs
+++ Pr04/Form1.cs
@@ -16,5 +16,35 @@ namespace Pr04
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime Date = Convert.ToDateTime(dateTimePicker1.Text);
+            string den = Date.DayOfWeek.ToString();
+
+
+            // MessageBox.Show("Dnes je den " + den); // den vypisuje dny
+
+            string zprava = string.Empty;
+            switch (den)
+            {
+                case "Monday":
+                    {
+                        zprava = den + " je první den v týdnu";
+                        break;
+                    }
+                case "Sunday":
+                    {
+                        zprava = den + " je poslední den v týdnu";
+                        break;
+                    }
+                default:
+                    {
+                        zprava = den + " Nejedná se o první ani poslední den v týdnu ";
+                        break;
+                    }
+            }
+            MessageBox.Show(zprava);
+        }
     }
 }
