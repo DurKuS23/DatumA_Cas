d6e7db54bfb69a2fe0b4cfba7c27f22c1cca8cf8
Daniel Nováček <Danny230147@seznam.cz>
2022-11-07 17:01:18 +0100

    Přidejte soubory projektu.

---
+++ Pr01/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime today = DateTime.Now;
+            MessageBox.Show("Dnes je " + today.DayOfWeek + " " + today.Date.ToString("dd/MMMM/yyyy") + " Je to " + today.DayOfYear + ". den v roce");
+            MessageBox.Show("Je " + today.Hour + " hodin " + today.Minute + " minut " + today.Second + " sekund");
+        }
+    }
+}
