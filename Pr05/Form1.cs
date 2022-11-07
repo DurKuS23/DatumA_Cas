cace4c1d66e44f1362ee5249a988605398d908a6
Daniel Nováček <Danny230147@seznam.cz>
2022-11-07 17:16:50 +0100

    Pr5_ENDED

--- Pr05/Form1.cs
+++ Pr05/Form1.cs
@@ -16,5 +16,33 @@ namespace Pr05
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string jmeno = string.Empty;
+            string prijmeni = string.Empty;
+            string datum = string.Empty;
+            string[] slova;
+
+            DateTime Date;
+            DateTime Oldest = DateTime.Today;
+
+            for (int k = 0; k < textBox1.Lines.Count(); k++)
+            {
+                string Info = textBox1.Lines[k];
+                slova = Info.Split(';');
+
+                Date = Convert.ToDateTime(slova[2]);
+
+                if (Date < Oldest)
+                {
+                    jmeno = slova[0];
+                    prijmeni = slova[1];
+                    Oldest = Date;
+                }
+            }
+            MessageBox.Show("Nejstarší " + jmeno + " " + prijmeni + " je narozen " + Oldest.ToString("d"));
+
+        }
     }
 }
